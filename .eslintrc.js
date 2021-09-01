550b8ef54734c3010ff2b20f8da208656d721749
lyana <lyana-m@yandex.ru>
2021-09-01 17:02:13 +0500

    реализована фильтрация товаров по цене и категории

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,14 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    "plugin:vue/essential",
-    "@vue/airbnb"
-  ],
+  extends: ["plugin:vue/essential", "@vue/airbnb"],
   parserOptions: {
     parser: "babel-eslint"
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "comma-dangle": ["error", "never"],
+    // "comma-dangle": ["error", "never"],
     quotes: [2, "double"]
   }
 };
--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,79 @@
 <template>
-  <section class="catalog">
-    <ProductList :products="products" />
-    <BasePagination v-model="page" :total-count="countProducts" :items-per-page="productsPerPage" />
-  </section>
+  <main class="content container">
+    <div class="content__top content__top--catalog">
+      <h1 class="content__title">Каталог</h1>
+      <span class="content__info"> 152 товара </span>
+    </div>
+
+    <div class="content__catalog">
+      <ProductFilter
+        :priceFrom.sync="filterPriceFrom"
+        :priceTo.sync="filterPriceTo"
+        :categoryId.sync="filterCategoryId"
+      />
+
+      <section class="catalog">
+        <ProductList :products="products" />
+        <BasePagination
+          v-model="page"
+          :total-count="countProducts"
+          :items-per-page="productsPerPage"
+        />
+      </section>
+    </div>
+  </main>
 </template>
 
 <script>
 import productsItems from "./data/products";
 import ProductList from "./components/ProductList.vue";
 import BasePagination from "./components/BasePagination.vue";
+import ProductFilter from "./components/ProductFilter.vue";
 
 export default {
   name: "App",
-  components: { ProductList, BasePagination },
+  components: { ProductList, BasePagination, ProductFilter },
   data() {
     return {
+      filterPriceFrom: 0,
+      filterPriceTo: 0,
+      filterCategoryId: 0,
+
       page: 1,
       productsPerPage: 3,
-      productsItems
     };
   },
   computed: {
+    filteredProducts() {
+      let filteredProducts = productsItems;
+
+      if (this.filterPriceFrom > 0) {
+        filteredProducts = filteredProducts.filter(
+          (product) => product.price > this.filterPriceFrom,
+        );
+      }
+
+      if (this.filterPriceTo > 0) {
+        filteredProducts = filteredProducts.filter(
+          (product) => product.price < this.filterPriceTo,
+        );
+      }
+
+      if (this.filterCategoryId) {
+        filteredProducts = filteredProducts.filter(
+          (product) => product.categoryId === this.filterCategoryId,
+        );
+      }
+
+      return filteredProducts;
+    },
     products() {
       const offcet = (this.page - 1) * this.productsPerPage;
-      return productsItems.slice(offcet, offcet + this.productsPerPage);
+      return this.filteredProducts.slice(offcet, offcet + this.productsPerPage);
     },
     countProducts() {
-      return productsItems.length;
-    }
-  }
+      return this.filteredProducts.length;
+    },
+  },
 };
 </script>
