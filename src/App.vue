668c6adae3ca348ef39ab0f8708f24c0707c9c89
lyana <lyana-m@yandex.ru>
2021-09-01 13:25:06 +0500

    реализована пагинация

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,18 @@
 <template>
   <section class="catalog">
-    <ProductList :products="products"></ProductList>
-
-    <ul class="catalog__pagination pagination">
-      <li class="pagination__item">
-        <a
-          class="
-            pagination__link pagination__link--arrow pagination__link--disabled
-          "
-          aria-label="Предыдущая страница"
-        >
-          <svg width="8" height="14" fill="currentColor">
-            <use xlink:href="#icon-arrow-left"></use>
-          </svg>
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link pagination__link--current"> 1 </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#"> 2 </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#"> 3 </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#"> 4 </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#"> ... </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#"> 10 </a>
-      </li>
-      <li class="pagination__item">
-        <a
-          class="pagination__link pagination__link--arrow"
-          href="#"
-          aria-label="Следующая страница"
-        >
-          <svg width="8" height="14" fill="currentColor">
-            <use xlink:href="#icon-arrow-right"></use>
-          </svg>
-        </a>
-      </li>
-    </ul>
+    <ProductList :products="products" />
+    <BasePagination v-model="page" :total-count="countProducts" :items-per-page="productsPerPage" />
   </section>
 </template>
 
 <script>
 import productsItems from "./data/products";
 import ProductList from "./components/ProductList.vue";
+import BasePagination from "./components/BasePagination.vue";
 
 export default {
   name: "App",
-  components: { ProductList },
+  components: { ProductList, BasePagination },
   data() {
     return {
       page: 1,
@@ -66,6 +24,9 @@ export default {
     products() {
       const offcet = (this.page - 1) * this.productsPerPage;
       return productsItems.slice(offcet, offcet + this.productsPerPage);
+    },
+    countProducts() {
+      return productsItems.length;
     }
   }
 };
---
+++ src/components/BasePagination.vue
@@ -0,0 +1,62 @@
+<template>
+  <ul class="catalog__pagination pagination">
+    <li class="pagination__item">
+      <a
+        class="pagination__link pagination__link--arrow"
+        :class="{ 'pagination__link--disabled': page === 1 }"
+        @click.prevent="paginate(page - 1)"
+        href="#"
+        aria-label="Предыдущая страница"
+      >
+        <svg width="8" height="14" fill="currentColor">
+          <use xlink:href="#icon-arrow-left"></use>
+        </svg>
+      </a>
+    </li>
+    <li v-for="pageNumber in pages" :key="pageNumber" class="pagination__item">
+      <a
+        href="#"
+        class="pagination__link"
+        :class="{ 'pagination__link--current': pageNumber === page }"
+        @click.prevent="paginate(pageNumber)"
+      >
+        {{ pageNumber }}
+      </a>
+    </li>
+    <li class="pagination__item">
+      <a
+        class="pagination__link pagination__link--arrow"
+        :class="{ 'pagination__link--disabled': page === pages }"
+        @click.prevent="paginate(page + 1)"
+        href="#"
+        aria-label="Следующая страница"
+      >
+        <svg width="8" height="14" fill="currentColor">
+          <use xlink:href="#icon-arrow-right"></use>
+        </svg>
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: "page",
+    event: "paginate"
+  },
+  props: ["page", "totalCount", "itemsPerPage"],
+  methods: {
+    paginate(page) {
+      this.$emit("paginate", page);
+    }
+  },
+  computed: {
+    pages() {
+      return Math.ceil(this.totalCount / this.itemsPerPage);
+    }
+  }
+};
+</script>
+
+<style></style>
