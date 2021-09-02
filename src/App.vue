6ef3626d8d488f0e8770ff8a0b0786f72e6f2a5d
lyana <lyana-m@yandex.ru>
2021-09-02 17:21:07 +0500

    реализована маршрутизация c использованием vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,7 @@
 <template>
-  <component :is="currentPageComponent" :page-params="currentPageParams" />
+  <router-view />
 </template>
 
 <script>
-import MainPage from './pages/MainPage.vue';
-import ProductPage from './pages/ProductPage.vue';
-import NotFoundPage from './pages/NotFoundPage.vue';
-import eventBus from './eventBus';
-
-const routes = {
-  main: 'MainPage',
-  product: 'ProductPage',
-};
-
-export default {
-  components: { MainPage, ProductPage, NotFoundPage },
-  data() {
-    return {
-      currentPage: 'main',
-      currentPageParams: {},
-    };
-  },
-  methods: {
-    goToPage(pageName, pageParams) {
-      this.currentPage = pageName;
-      this.currentPageParams = pageParams || {};
-    },
-  },
-  computed: {
-    currentPageComponent() {
-      return routes[this.currentPage] || 'NotFoundPage';
-    },
-  },
-  created() {
-    eventBus.$on('goToPage', (pageName, pageParams) =>
-      this.goToPage(pageName, pageParams),
-    );
-  },
-};
+export default {};
 </script>
--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -1,12 +1,11 @@
 <template>
   <li class="catalog__item">
-    <a
+    <router-link
       class="catalog__pic"
-      href="#"
-      @click.prevent="goToPage('product', { id: product.id })"
+      :to="{ name: 'product', params: { id: product.id } }"
     >
       <img :src="product.image" :alt="product.title" />
-    </a>
+    </router-link>
 
     <h3 class="catalog__title">
       <a href="#">
@@ -36,7 +35,6 @@
 </template>
 
 <script>
-import goToPage from '../helpers/goToPage';
 import numberFormat from '../helpers/numberFormat';
 
 export default {
@@ -48,9 +46,6 @@ export default {
   },
   filters: {
     numberFormat,
-  },
-  methods: {
-    goToPage,
   },
 };
 </script>
